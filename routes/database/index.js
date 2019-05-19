6043f28765caef7e292b4ebdf893099d96489cac
Seth Feder <sethfeder2017@u.northwestern.edu>
2019-05-19 13:01:26 -0400

    set up backend for blood pressure measurements

--- routes/database/index.js
+++ routes/database/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const resultRoutes = require("./resultRoutes")
+const bpRoutes = require("./bpRoutes")
 // const dialogueRoutes = require("./dialogueRoutes");
 
 router.use("/users", userRoutes);
 // router.use("/dialogue", dialogueRoutes);
 router.use("/results", resultRoutes);
+router.use("/bp", bpRoutes);
 
 
 module.exports = router;
